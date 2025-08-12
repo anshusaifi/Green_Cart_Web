fb85127efcc8c1f0322a9160e10aefc0a5f00c73
anshusaifi <saifianash&@gmail.com>
2025-08-13 00:30:08 +0530

    update url for live deployement

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Change to your backend URL in production
+  baseURL: "https://greencart12.netlify.app/api", // Change to your backend URL in production
   withCredentials: true, // send cookies (JWT token in cookie)
   headers: {
     "Content-Type": "application/json",
